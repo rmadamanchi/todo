83c5a902331969d6b17821916ee50d8eeab366a8
Rajesh Madamanchi <rmadamanchi@gmail.com>
2018-11-27 07:01:35 -0600

    added more handlers

--- tasks/handler.go
+++ tasks/handler.go
@@ -4,17 +4,36 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 var repository = NewRepository(MemoryMap)
 
 func RegisterHandlers(router *mux.Router) {
 	router.HandleFunc("", handleGetTasks).Methods("GET")
+	router.HandleFunc("", handlePostTask).Methods("POST")
+	router.HandleFunc("/{id}", handleGetTask).Methods("GET")
 }
 
-func handleGetTasks(writer http.ResponseWriter, _ *http.Request) {
-	writer.Header().Set("Content-Type", "application/json")
-	writer.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	writer.WriteHeader(http.StatusOK)
-	json.NewEncoder(writer).Encode(repository.all())
+func handleGetTasks(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(repository.all())
+}
+
+func handleGetTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 16)
+	if err == nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	task := repository.get(int16(id))
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(task)
+}
+
+func handlePostTask(w http.ResponseWriter, r *http.Request) {
+
 }
